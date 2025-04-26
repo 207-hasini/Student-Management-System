42ba45b8b1c72d85e88f9ff35b33d73d7b2de886
hasini <h.rose7760@gmail.com>
2025-04-26 19:31:49 +0530

    initial commit

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">Student MS</Link>
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav" 
+          aria-controls="navbarNav" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className={({isActive}) => isActive ? "nav-link active" : "nav-link"} to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className={({isActive}) => isActive ? "nav-link active" : "nav-link"} to="/students">
+                Students
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className={({isActive}) => isActive ? "nav-link active" : "nav-link"} to="/students/add">
+                Add Student
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
